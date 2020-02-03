6add0f44250060327f51b693892d03311bbb19c3
tom <11509310+tomzs@users.noreply.github.com>
2020-02-03 19:15:14 +0200

    Front and Back boilerplate, axios

---
+++ client/src/components/App/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import expressApi from '../../apis/expressApi'
+const App = () => {
+  const [ apiResponse, setApiResponse ] = React.useState(null)
+
+  const apiRes = async () => {
+    const res = await expressApi.get('/test')
+    setApiResponse(res.data)
+  }
+
+  React.useEffect(() => {
+    apiRes()
+  }, [])
+
+  return (
+    <div>
+      <h1>React is up and running</h1>
+      <h3>{apiResponse}</h3>
+    </div>
+  )
+}
+
+export default App
